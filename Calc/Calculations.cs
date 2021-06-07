a5ba8a2f7455e8ae30294374c62090381ef25b1d
metlin.amir@mail.ru <metlin.amir@mail.ru>
2021-06-07 21:59:10 +0300

    07.06.21

--- Calc/Calculations.cs
+++ Calc/Calculations.cs
@@ -8,28 +8,25 @@ namespace Calc
 {
     public class Calculations
     {
-       public static double Calculator(double a, double b,int index)
+       public static double Calculator(string matExpression)
         {
-            double res =0;
-            switch (index)
-            {
-                case 0:
-                    res = a + b;
-                    return res;
-                case 1:
-                    res = a - b;
-                    return res;
-                case 2:
-                    res = a * b;
-                    return res;
-                case 3:
-                    res = a / b;
-                    return res;
-                case 4:
-                    res = Math.Pow(a, b);
-                    return res;
-            }
-            return res ;
+            //matExpression.Replace("X", "*");
+            int found0 = matExpression.LastIndexOf("+");
+            if (found0 >= 0)
+                return Calculator(matExpression.Substring(0, found0)) + Calculator(matExpression.Substring(found0 + 1));
+            int found1 = matExpression.LastIndexOf("-");
+            if (found1 >= 0)
+                return Calculator(matExpression.Substring(0, found1)) - Calculator(matExpression.Substring(found1 + 1));
+            int found2 = matExpression.LastIndexOf("x");
+            if (found2 >= 0)
+                return Calculator(matExpression.Substring(0, found2)) * Calculator(matExpression.Substring(found2 + 1));
+            int found3 = matExpression.LastIndexOf(":");
+            if (found3 >= 0)
+                return Calculator(matExpression.Substring(0, found3)) / Calculator(matExpression.Substring(found3 + 1));
+            int found4 = matExpression.LastIndexOf("^");
+            if (found4 >= 0)
+                return Math.Pow(Calculator(matExpression.Substring(0, found4)), Calculator(matExpression.Substring(found4 + 1)));
+            return Convert.ToDouble(matExpression);
         }
     }
 }
--- Calc/MainWindow.xaml.cs
+++ Calc/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace Calc
 {
@@ -23,23 +24,212 @@ namespace Calc
         public MainWindow()
         {
             InitializeComponent();
+            tbActions.Text = "";
+            tbCalculations.Text = "";
         }
 
-        private void btnCount_Click(object sender, RoutedEventArgs e)
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
         {
-            if(cbSimbol.SelectedIndex==-1||tbNum1.Text=="" || tbNum2.Text == "")
+            tbCalculations.Text = tbCalculations.Text + 7;
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 8;
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 9;
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 4;
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 5;
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 6;
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 1;
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 2;
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 3;
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = tbCalculations.Text + 0;
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalculations.Text = "";
+            tbActions.Text = "";
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (tbCalculations.Text != "")
             {
-                MessageBox.Show("Введите все данные!");
+                tbCalculations.Text = tbCalculations.Text + ":";
             }
             else
-            { 
-            double a = Convert.ToDouble(tbNum1.Text);
-            double b = Convert.ToDouble(tbNum2.Text);
-                tbResult.Text =Convert.ToString(Calculations.Calculator(a, b, cbSimbol.SelectedIndex));
-
-
-
+            {
+                return;
             }
+
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCalculations.Text != "")
+            {
+                tbCalculations.Text = tbCalculations.Text + "x";
+            }
+            
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCalculations.Text != "")
+            {
+                tbCalculations.Text = tbCalculations.Text + "-";
+            }
+            
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCalculations.Text != "")
+            {
+                tbCalculations.Text = tbCalculations.Text + "+";
+            }
+            
+        }
+
+        private void btnComma_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCalculations.Text != "")
+            {
+                tbCalculations.Text = tbCalculations.Text + ",";
+            }
+            
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            tbActions.Text = "";
+            tbCalculations.Text = tbCalculations.Text.Remove(tbCalculations.Text.Length - 1);
+        }
+
+        private void btnPow_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCalculations.Text != "")
+            {
+                tbCalculations.Text = tbCalculations.Text + "^";
+            }
+            
+        }
+
+        private void btnEquate_Click(object sender, RoutedEventArgs e)
+        {
+            tbActions.Text = tbCalculations.Text;
+            tbCalculations.Text = Calculations.Calculator(tbCalculations.Text).ToString();
+            
+        }
+
+        int a = 0;
+        private void tbCalculations_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            if(a>0)
+            {
+
+            
+            
+                    btnComma.IsEnabled = true;
+                    btnDivide.IsEnabled = true;
+                    btnPow.IsEnabled = true;
+                    btnMinus.IsEnabled = true;
+                    btnMultiply.IsEnabled = true;
+                    btnPlus.IsEnabled = true;
+
+                if (tbCalculations.Text.Length!=0 && tbCalculations.Text[tbCalculations.Text.Length - 1] == ':')
+                {
+                    btnDivide.IsEnabled = false;
+                    btnComma.IsEnabled = false;
+                    btnPow.IsEnabled = false;
+                    btnMinus.IsEnabled = false;
+                    btnMultiply.IsEnabled = false;
+                    btnPlus.IsEnabled = false;
+                }
+                if (tbCalculations.Text.Length != 0 && tbCalculations.Text[tbCalculations.Text.Length - 1] == '+')
+                {
+                    btnDivide.IsEnabled = false;
+                    btnComma.IsEnabled = false;
+                    btnPow.IsEnabled = false;
+                    btnMinus.IsEnabled = false;
+                    btnMultiply.IsEnabled = false;
+                    btnPlus.IsEnabled = false;
+                }
+                if (tbCalculations.Text.Length != 0 && tbCalculations.Text[tbCalculations.Text.Length - 1] == '-')
+                {
+                    btnDivide.IsEnabled = false;
+                    btnComma.IsEnabled = false;
+                    btnPow.IsEnabled = false;
+                    btnMinus.IsEnabled = false;
+                    btnMultiply.IsEnabled = false;
+                    btnPlus.IsEnabled = false;
+                }
+                if (tbCalculations.Text.Length != 0 && tbCalculations.Text[tbCalculations.Text.Length - 1] == 'x')
+                {
+                    btnDivide.IsEnabled = false;
+                    btnComma.IsEnabled = false;
+                    btnPow.IsEnabled = false;
+                    btnMinus.IsEnabled = false;
+                    btnMultiply.IsEnabled = false;
+                    btnPlus.IsEnabled = false;
+                }
+                if (tbCalculations.Text.Length != 0 && tbCalculations.Text[tbCalculations.Text.Length - 1] == '^')
+                {
+                    btnDivide.IsEnabled = false;
+                    btnComma.IsEnabled = false;
+                    btnPow.IsEnabled = false;
+                    btnMinus.IsEnabled = false;
+                    btnMultiply.IsEnabled = false;
+                    btnPlus.IsEnabled = false;
+                }
+                if (tbCalculations.Text.Length != 0 && tbCalculations.Text[tbCalculations.Text.Length - 1] == ',')
+                {
+                    btnDivide.IsEnabled = false;
+                    btnComma.IsEnabled = false;
+                    btnPow.IsEnabled = false;
+                    btnMinus.IsEnabled = false;
+                    btnMultiply.IsEnabled = false;
+                    btnPlus.IsEnabled = false;
+                }
+            }
+            a++;
+
         }
     }
 }
